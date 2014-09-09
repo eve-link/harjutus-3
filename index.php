2f2c3febc1181dbdf273699ce49dde1fa8ebe6ce
eve-link <eve.link.1992@gmail.com>
2014-09-09 15:10:54 +0300

    Add sample variables

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head> 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Harjutus-3</title>
+</head>
+
+<body> 
+<?php
+    $variable = 10;
+    echo $variable;
+    echo "<br>";
+?>
+<?php
+    $variable = 20;
+    echo $variable;
+?>
+
+</body>
+</html> 
